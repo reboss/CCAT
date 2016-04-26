f490fa37d2e642ffd54433eaba68df667a77946f
John Robbins <mulvanyrobbinsj@mymacewan.ca>
2016-04-26 17:01:02 -0600

    Add file to load questions from database instead of txt file

--- CCAT/src/database/SQLiteCreate.java
+++ CCAT/src/database/SQLiteCreate.java
@@ -2,6 +2,13 @@
 
 package database;
 
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
