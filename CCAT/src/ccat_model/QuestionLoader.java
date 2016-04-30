01e9eeb3197ce9da4e27722e91bf04db09b38cba
John Robbins <mulvanyrobbinsj@mymacewan.ca>
2016-04-29 20:15:46 -0600

    Load in questions from database

--- CCAT/src/ccat_model/QuestionLoader.java
+++ CCAT/src/ccat_model/QuestionLoader.java
@@ -1,5 +1,10 @@
 package ccat_model;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
@@ -14,18 +19,59 @@ public class QuestionLoader {
 
     private final Map<String, Map<String, List<String>>> content;
     private Map<String, List<String>> orderedSubheaders;
+    private Connection dbConnection;
 
     /**
      * 
      */
-    public QuestionLoader() {
+    public QuestionLoader() throws SQLException {
         content = new HashMap<>();
+        dbConnection = null;
+        
+        dbConnection = DriverManager.getConnection("jdbc:sqlite:CCAT.db");
+        dbConnection.setAutoCommit(false);
+        
+        
     }
 
     /**
      * 
      */
-    public void loadQuestions() {
+    public void loadQuestions() throws SQLException {
+        
+        Statement stmt = null;
+        stmt = dbConnection.createStatement();
+        String sql = "SELECT id, part, header FROM headers";
+        ResultSet headerQueryResults = stmt.executeQuery(sql);
+        
+        while (headerQueryResults.next()){
+            
+            int part = headerQueryResults.getInt("part");
+            String header = headerQueryResults.getString("header");
+            
+            if (content.get(part) == null){
+                content.put("Part "+ part, new HashMap<>());
+                orderedSubheaders.put("Part "+part, new ArrayList<>());
+            }
+            
+            sql = "SELECT question, hid, FROM questions WHERE hid = " + headerQueryResults.getInt("id");
+            ResultSet questionsQueryResults = stmt.executeQuery(sql);
+            
+            while (questionsQueryResults.next()){
+                
+                if (content.get(part).get(header) == null){
+                    content.get(part).put(header, new ArrayList<>());
+                    orderedSubheaders.get(part).add(header);
+                }
+                
+                String question = questionsQueryResults.getString("question");                
+                content.get(part).get(header).add(question);
+                
+            }
+            
+            
+
+        }
     }
 
     /**
