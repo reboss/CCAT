c14f2e056afe727f02ad0a00591a29ab89e8a241
Elliott Sobek <elliottsobek@gmail.com>
2016-04-29 21:53:26 -0600

    add copyright headers

--- CCAT/src/ccat_model/FileLoader.java
+++ CCAT/src/ccat_model/FileLoader.java
@@ -1,3 +1,11 @@
+/**
+ * Copyright (C) John Mulvany-Robbins, Elliott Sobek, Zac Batog.
+ * Github profiles:
+ * John Mulvany-Robbins (https://github.com/reboss),
+ * Elliott Sobek (https://github.com/ElliottSobek),
+ * Zac Batog (https://github.com/batogz) 
+ */
+
 package ccat_model;
 
 import java.io.FileNotFoundException;
