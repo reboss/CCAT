6ca347f5766bdb2a31f98493f8d2cd2818998c27
Elliott Sobek <elliottsobek@gmail.com>
2016-04-30 22:21:13 -0600

    Adjust formatting, fix imports, improve code quality

--- CCAT/src/ccat_model/AnswerModel.java
+++ CCAT/src/ccat_model/AnswerModel.java
@@ -1,16 +1,20 @@
+/**
+ * Copyright (C) John Robbins, Elliott Sobek, Zac Batog.
+ * Github profiles:
+ * John Robbins (https://github.com/reboss),
+ * Elliott Sobek (https://github.com/ElliottSobek),
+ * Zac Batog (https://github.com/batogz) 
+ */
+
 package ccat_model;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-
 import java.util.ArrayList;
-
-
 
 /**
  *
@@ -18,33 +22,46 @@ import java.util.ArrayList;
  */
 public class AnswerModel {
 
-    private Connection connection;
-    
-    public AnswerModel() throws SQLException{
-        connection = DriverManager.getConnection("jdbc:sqlite:CCAT.db");
+    private final Connection connection;
 
+    /**
+     *
+     * @throws SQLException
+     */
+    public AnswerModel() throws SQLException {
+        connection = DriverManager.getConnection("jdbc:sqlite:CCAT.db");
         connection.setAutoCommit(false);
     }
-    
-    public void saveAnswers(List<String> answers, int userID) throws SQLException{
-        
+
+    /**
+     *
+     * @param answers
+     * @param userID
+     * @throws SQLException
+     */
+    public void saveAnswers(List<String> answers, int userID) throws SQLException {
+
         Statement stmt = connection.createStatement();
         int user = userID;
         Date date = new Date();
         String qid = "(SELECT id FROM questions WHERE question = ";
-        String sql =    "INSERT INTO answers ()"+
-                        "VALUES ()";
-
+        String sql = "INSERT INTO answers ()"
+                + "VALUES ()";
 
     }
-    
-    public void saveAnswers(ArrayList<String> answers){
 
+    /**
+     *
+     * @param answers
+     */
+    public void saveAnswers(ArrayList<String> answers) {
 
-        
     }
-    
-    public void retrieveAnswers(){
-        
+
+    /**
+     *
+     */
+    public void retrieveAnswers() {
+
     }
 }
--- CCAT/src/ccat_model/QuestionLoader.java
+++ CCAT/src/ccat_model/QuestionLoader.java
@@ -30,7 +30,7 @@ public class QuestionLoader {
     private final Connection dbConnection;
 
     /**
-     * 
+     *
      * @throws java.sql.SQLException
      */
     public QuestionLoader() throws SQLException {
@@ -38,53 +38,50 @@ public class QuestionLoader {
         orderedSubheaders = new HashMap<>();
         dbConnection = DriverManager.getConnection("jdbc:sqlite:CCAT.db");
         dbConnection.setAutoCommit(false);
-        
-        
+
     }
 
     /**
-     * 
+     *
      * @throws java.sql.SQLException
      */
     public void loadQuestions() throws SQLException {
-        
+
         Statement stmt1 = dbConnection.createStatement();
         Statement stmt2 = dbConnection.createStatement();
         String sql = "SELECT id, part, header FROM headers";
         ResultSet headerQueryResults = stmt1.executeQuery(sql);
-        
-        
-        while (headerQueryResults.next()){
-            
+
+        while (headerQueryResults.next()) {
+
             int part = headerQueryResults.getInt("part");
             String header = headerQueryResults.getString("header");
-            
+
             Map<String, List<String>> questions = new HashMap<>();
             List<String> headers = new ArrayList<>();
-            
-            if (content.get("Part "+part) == null){
-                
-                content.put("Part "+ part, questions);
-                orderedSubheaders.put("Part "+part, headers);
-                
+
+            if (content.get("Part " + part) == null) {
+
+                content.put("Part " + part, questions);
+                orderedSubheaders.put("Part " + part, headers);
+
             }
-            
-            if (content.get("Part "+part).get(header) == null){
-                
-                content.get("Part "+part).put(header, new ArrayList<>());
-                orderedSubheaders.get("Part "+part).add(header);
-                
+
+            if (content.get("Part " + part).get(header) == null) {
+
+                content.get("Part " + part).put(header, new ArrayList<>());
+                orderedSubheaders.get("Part " + part).add(header);
+
             }
-            
+
             sql = "SELECT question, hid FROM questions WHERE hid = " + headerQueryResults.getInt("id");
             ResultSet questionsQueryResults = stmt2.executeQuery(sql);
-            
-            while (questionsQueryResults.next()){              
-                
-                String question = questionsQueryResults.getString("question");                
-                content.get("Part "+part).get(header).add(question);
-                
-                
+
+            while (questionsQueryResults.next()) {
+
+                String question = questionsQueryResults.getString("question");
+                content.get("Part " + part).get(header).add(question);
+
             }
 
         }
@@ -92,15 +89,15 @@ public class QuestionLoader {
     }
 
     /**
-     * 
-     * @return 
+     *
+     * @return
      */
     public Map<String, Map<String, List<String>>> getContent() {
         return Collections.unmodifiableMap(content);
     }
 
     /**
-     * 
+     *
      */
     public void traverseMap() {
         for (String header : orderedSubheaders.keySet()) {
@@ -115,16 +112,16 @@ public class QuestionLoader {
     }
 
     /**
-     * 
-     * @return 
+     *
+     * @return
      */
     public Map<String, List<String>> getOrderedSubheaders() {
         return Collections.unmodifiableMap(orderedSubheaders);
     }
 
     /**
-     * 
-     * @return 
+     *
+     * @return
      */
     public List<String> getHeaders() {
         List<String> headers = new ArrayList<>();
