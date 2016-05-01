8545cc2aad4c18ef171a51d4d260fb23e2eb443b
Elliott Sobek <elliottsobek@gmail.com>
2016-04-30 22:36:54 -0600

    Increase about page font size

--- CCAT/src/ccat_view/MainMenu/AboutController.java
+++ CCAT/src/ccat_view/MainMenu/AboutController.java
@@ -15,6 +15,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 /**
@@ -48,6 +49,7 @@ public class AboutController implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        info.setFont(Font.font(18.0));
         info.setText("The Critical Care Audit Tool (CCAT) is an evidenced-based, self-educating audit tool "
                 + "for Registered Nurses of the ICU and CCU units at the Grey Nuns Hospital to increase "
                 + "their knowledge in documentation while increasing their patient care competencies, care "
