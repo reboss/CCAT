970eb77f942214310880b99f637f152fe0b7486e
John Robbins <mulvanyrobbinsj@mymacewan.ca>
2016-05-05 16:04:44 -0600

    Add classes for Questions, Answers and Headers

---
+++ CCAT/src/ccat_model/Answer.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ccat_model;
+
+/**
+ *
+ * @author JRebo_000
+ */
+public class Answer extends CCATQA{
+
+    public Answer(String objectText, Integer objectId, Integer parentId) {
+        super(objectText, objectId, parentId);
+    }
+    
+}
